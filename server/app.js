78b4e62f2674f8eaf058a6f0f12ddbeee55e3e80
santycano <santy.cano@hotmail.com>
2021-11-05 18:50:08 -0500

    Heroku 3

--- server/app.js
+++ server/app.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use('/api', routes);
 
 // error handling
-app.use((error, req, res) => {
+app.use((error, req, res, next) => {
   res.status(error.status || 500).json({
     status: Number(error.status) || 500,
     message: error.message
